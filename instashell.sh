373b590ea3b6e722df2908f1ae53d1bf5a650694
knbniniwxsm <baurzhanovnanaira2@gmail.com>
2025-04-17 20:32:56 -0400

    Update instashell.sh

--- instashell.sh
+++ instashell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Coded by @thelinuxchoice (Instagram)
+# Coded by @_darxit_ (Instagram)
 checkroot() {
 if [[ "$(id -u)" -ne 0 ]]; then
     printf "\e[1;77mPlease, run this program as root!\n\e[0m"
@@ -16,7 +16,7 @@ printf "\e[1;92m ) )| ||  _ \  /___)(_   _)(____ | /___)|  _ \ | ___ || || |    
 printf "\e[1;77m(_/ | || | | ||___ |  | |_ / ___ ||___ || | | || ____|| || |  _____   \e[0m\n"
 printf "\e[1;77m    |_||_| |_|(___/    \__)\_____|(___/ |_| |_||_____) \_)\_)(_____)  \e[0m\n"
 printf "\n"
-printf "\e[1;77m\e[45m                     Instagram Brute Forcer. Author: @thelinuxchoice\e[0m\n"
+printf "\e[1;77m\e[45m                     Instagram Brute Forcer. Author: @darxit\e[0m\n"
 printf "\n"
 }
 
